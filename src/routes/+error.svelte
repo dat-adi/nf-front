6b33a8e8e75edce377487a5f981dd8bc0d797d96
dat-adi <dat.adithya@gmail.com>
2023-07-05 01:25:02 +0530

    feat: Error page
    Set up an error page for the application to default to.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+    <div class="space-y-5">
+        <h1 class="h1 font-princess-sofia"> This page might not exist.</h1>
+        <h2 class="h2 font-princess-sofia">Head <a class="anchor" href="/">back?</a> </h2>
+    </div>
+</div>
